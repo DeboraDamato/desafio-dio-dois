46056e543058d0c0db48201a95f5b6916becf650
DeboraDamato <debora_damato@hotmail.com>
2024-02-25 14:10:46 -0300

    Finalização do desafio 2

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Olá")
+let totalGeral = rankingCalculator(102, 1)
+let nivel;
+
+function rankingCalculator(qtdVitoria, qtdDerrota){
+
+    let totalVitoria = qtdVitoria - qtdDerrota
+    return totalVitoria
+}
+if (totalGeral <= 10){
+    nivel = "Ferro";
+} else if (totalGeral  <= 20){
+    nivel = "Bronze";
+} else if (totalGeral  <= 50){
+    nivel = "Prata";
+} else if (totalGeral  <= 80){
+    nivel = "Ouro";
+} else if (totalGeral  <= 90){
+    nivel = "Diamante";
+} else if (totalGeral  <= 100){
+    nivel = "Lendário";
+} else {
+    nivel = "Imortal";
+}
+console.log(`Saldo de vitórias é ${totalGeral} está no nível ${nivel}`);
